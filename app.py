7f950388cd5d98187c5a5abb9a935a83d2f48092
JunKaiWong <junkaiwong@ymail.com>
2023-10-05 23:27:07 +0800

    a

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, url_for
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("blog.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
