7c6eb0bbe18e95f6db782085cfd9c19e59038ed0
CCherryl <71059023+CCherryl@users.noreply.github.com>
2023-10-07 01:52:43 +0800

    display data

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+import csv
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("products.html")
+    with open("NLP_Dataset_Edits.csv", encoding="utf8") as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        return render_template("products.html", header=header, rows=reader)
 
 @app.route("/search", methods = ['GET', 'POST'])
 def search():
