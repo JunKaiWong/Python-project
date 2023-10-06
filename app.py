a224d6cad8636bb0194139b34500fe566c385448
JunKaiWong <junkaiwong@ymail.com>
2023-10-07 02:32:37 +0800

    search function 75% done

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_bootstrap import Bootstrap
+import pandas as pd
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("products.html")
+    return render_template("index.html")
 
-@app.route("/search", methods = ['GET', 'POST'])
+@app.route("/search", methods=["POST", "GET"] )
 def search():
-    return render_template("search.html")
+    if request.method == 'POST':
+        search_query = request.form.get('dataresult')
+        results = []
+        data = pd.read_pickle('NLP_Dataset.pkl')
+        print(search_query)
+        for index in data.index:
+            product_name = data['Product'][index]
+            if search_query in product_name:
+                results.append(data.iloc[index])
+                
+        lengthlist= len(results)
+        print(results)
+        print(lengthlist)
+    return render_template("search.html", results = results, lengthlist=lengthlist)
 
 
 if __name__ == '__main__':
